282624af47bfa7d2b9ccac8f196b59245f392bbb
Onui Hamza <ouni.hamza119612@live.fr>
2022-07-11 16:30:47 +0100

    commit init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import FullPage from "./components/FullPage";
+import PageImage from "./assets/images/image1.jpg";
 
 function App() {
-  return <div className="App">hello world!</div>;
+  return (
+    <div className="App">
+      <img src={PageImage} alt="Image" />
+    </div>
+  );
 }
 
 export default App;
